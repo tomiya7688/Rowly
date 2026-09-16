37c2cb6952b5638bc9cfebd221865ab19d5de31d
kadoka <tomiya3499@gmail.com>
2026-09-17 07:44:12 +0900

    expose edit core references

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -1,4 +1,7 @@
+mod address;
 mod document;
+mod history;
 
 pub use crate::data::SourceEncoding;
+pub use address::{CellRange, CellRef, ReferenceError};
 pub use document::{CsvDocument, DocumentError};

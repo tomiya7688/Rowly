efec5594cd1e8389749aec94220430dd4a112408
kadoka <tomiya3499@gmail.com>
2026-09-17 17:57:01 +0900

    apply rustfmt to process exports

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -6,7 +6,6 @@ mod history;
 pub use crate::data::SourceEncoding;
 pub use address::{CellRange, CellRef, ReferenceError};
 pub use column::{
-    ColumnCell, ColumnError, ColumnType, ColumnTypeReport, JapaneseCheckReport,
-    contains_japanese,
+    ColumnCell, ColumnError, ColumnType, ColumnTypeReport, JapaneseCheckReport, contains_japanese,
 };
 pub use document::{CsvDocument, DocumentError};

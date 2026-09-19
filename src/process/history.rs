0f029bce13790320f232979741ffbe63e09942e4
kadoka <tomiya3499@gmail.com>
2026-09-19 17:11:43 +0900

    transaction 中は undo redo を利用不可として公開

--- src/process/history.rs
+++ src/process/history.rs
@@ -68,11 +68,11 @@ impl EditHistory {
     }
 
     pub(super) fn can_undo(&self) -> bool {
-        !self.undo.is_empty()
+        self.transaction.is_none() && !self.undo.is_empty()
     }
 
     pub(super) fn can_redo(&self) -> bool {
-        !self.redo.is_empty()
+        self.transaction.is_none() && !self.redo.is_empty()
     }
 
     pub(super) fn record(&mut self, operation: EditOperation) {
